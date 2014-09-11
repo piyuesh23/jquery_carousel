1841040d1f33511e36fb6553b4855ec8f791c06a
piyuesh23 <piyuesh23@qed42.com>
2014-09-11 18:36:30 +0530

    Basic jquery carousel views plugin

---
+++ views/theme/jquery-carousel.tpl.php
@@ -0,0 +1,11 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Creates common class names between parents and children. Hides non active children. Creates class names for containers.
+ *
+ * @ingroup views_templates
+ */
+
+?>
