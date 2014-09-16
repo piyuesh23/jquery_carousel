cbaefdcc9119d3336464c7a94bb9e423dd879ee4
piyuesh23 <piyuesh23@qed42.com>
2014-09-16 11:53:43 +0530

    Fixing variable passing from views config to Javascript

--- js/jquery_carousel_init.js
+++ js/jquery_carousel_init.js
@@ -3,6 +3,7 @@
     attach: function (context) {
       $(document).ready(function(){
         var jcarouselSettings = Drupal.settings.jquery_carousel;
+        console.log(jcarouselSettings);
         $('.rs-carousel').carousel(jcarouselSettings);
       });
     }
