ece1c7e621b49678e07ff9f09df3fa37490114da
piyuesh23 <piyuesh23@qed42.com>
2014-09-16 12:44:42 +0530

    removing unnecessary code

--- js/jquery_carousel_init.js
+++ js/jquery_carousel_init.js
@@ -3,7 +3,6 @@
     attach: function (context) {
       $(document).ready(function(){
         var jcarouselSettings = Drupal.settings.jquery_carousel;
-        console.log(jcarouselSettings);
         $('.rs-carousel').carousel(jcarouselSettings);
       });
     }
