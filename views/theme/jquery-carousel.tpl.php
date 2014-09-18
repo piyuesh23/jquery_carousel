0d0ab9b3618ff57164da437b9f3a7c9ee445faca
piyuesh23 <piyuesh23@qed42.com>
2014-09-18 19:00:13 +0530

    Fixing coding standards

--- views/theme/jquery-carousel.tpl.php
+++ views/theme/jquery-carousel.tpl.php
@@ -1,9 +1,9 @@
 <?php
-// $Id$
-
 /**
  * @file
- * Creates common class names between parents and children. Hides non active children. Creates class names for containers.
+ * Creates common class names between parents and children.
+ *
+ * Hides non active children. Creates class names for containers.
  *
  * @ingroup views_templates
  */
