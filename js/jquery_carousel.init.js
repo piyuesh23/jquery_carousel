6a6e08290fe930feaea0cdf117461c294649d34a
piyuesh23 <piyuesh23@qed42.com>
2014-10-05 17:56:18 +0530

    Removing document ready state

--- js/jquery_carousel.init.js
+++ js/jquery_carousel.init.js
@@ -6,19 +6,17 @@
 (function ($) {
   Drupal.behaviors.jqueryCarousel = {
     attach: function (context) {
-      $(document).ready(function(){
-        var jcarouselSettings = Drupal.settings.jquery_carousel;
-        var keys = [];
-        for(var k in jcarouselSettings) {
-          keys.push(k);
+      var jcarouselSettings = Drupal.settings.jquery_carousel;
+      var keys = [];
+      for(var k in jcarouselSettings) {
+        keys.push(k);
+      }
+      for (var i = 0; i < keys.length; i++) {
+        var selector = '.' + keys[i];
+        if ($(selector).length > 0) {
+          $(selector).carousel(jcarouselSettings[keys[i]]);
         }
-        for (var i = 0; i < keys.length; i++) {
-          var selector = '.' + keys[i];
-          if ($(selector).length > 0) {
-            $(selector).carousel(jcarouselSettings[keys[i]]);
-          }
-        }
-      });
+      }
     }
   };
 })(jQuery);
