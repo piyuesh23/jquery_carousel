da9a782358d59babc6c720b3fe6dd95048f599c5
piyuesh23 <piyuesh23@qed42.com>
2014-10-07 16:06:27 +0530

    Replacing double quotes with singles & using behaviour contexts

--- js/jquery_carousel.init.js
+++ js/jquery_carousel.init.js
@@ -5,7 +5,7 @@
 
 (function ($) {
   Drupal.behaviors.jqueryCarousel = {
-    attach: function (context) {
+    attach: function (context, settings) {
       var jcarouselSettings = Drupal.settings.jquery_carousel;
       var keys = [];
       for(var k in jcarouselSettings) {
@@ -13,8 +13,8 @@
       }
       for (var i = 0; i < keys.length; i++) {
         var selector = '.' + keys[i];
-        if ($(selector).length > 0) {
-          $(selector).carousel(jcarouselSettings[keys[i]]);
+        if ($(selector, context).length > 0) {
+          $(selector, context).carousel(jcarouselSettings[keys[i]]);
         }
       }
     }
